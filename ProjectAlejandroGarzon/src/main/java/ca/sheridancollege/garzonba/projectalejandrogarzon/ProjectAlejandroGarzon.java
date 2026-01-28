85bcbcad69497099ed762dbe589a325876f09541
Alejandro Garzon <alejitogarbo@gmail.com>
2026-01-28 14:58:50 -0500

    added the addition feature on the app

--- ProjectAlejandroGarzon/src/main/java/ca/sheridancollege/garzonba/projectalejandrogarzon/ProjectAlejandroGarzon.java
+++ ProjectAlejandroGarzon/src/main/java/ca/sheridancollege/garzonba/projectalejandrogarzon/ProjectAlejandroGarzon.java
@@ -4,13 +4,40 @@
 
 package ca.sheridancollege.garzonba.projectalejandrogarzon;
 
+import java.util.Scanner;
+
 /**
  *
  * @author aleji
  */
-public class ProjectAlejandroGarzon {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class ProjectAlejandroGarzon {public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Welcome to the simple calculator app, please check the options below");
+        System.out.println("Enter 1 for Addition");
+        System.out.println("Enter 2 for Substraction");
+        System.out.println("Enter 3 for Multiplication");
+        System.out.println("Enter 4 for Division");
+        
+        Integer userSelection = scanner.nextInt();
+        
+        System.out.println("Enter the first number");
+        Double firstNumber = scanner.nextDouble();
+        
+        System.out.println("Enter the second number");
+        Double secondNumber = scanner.nextDouble();
+        
+        switch (userSelection) {
+            case 1:
+                addition(firstNumber, secondNumber);
+                
+            default:
+                System.out.println("ERROR");
+        }
+    }
+        
+        
+    public static Double addition(Double num1, Double num2) {
+        return num1 + num2;
     }
 }
